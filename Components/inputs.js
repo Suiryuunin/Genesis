036cf06d42c3142a1197d111dae8399450890ea1
Suiryuunin <150402244+Suiryuunin@users.noreply.github.com>
2023-11-20 11:25:24 -0500

    Erases everything with enter key

--- Components/inputs.js
+++ Components/inputs.js
@@ -34,6 +34,10 @@ const keyDown = (e) => {
                         
                     }
                     break;
+
+                case "Enter":
+                    display.input = "";
+                    break;
         
             }
     
